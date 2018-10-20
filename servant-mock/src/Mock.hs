dcff6bed7ced4cb9ee00a548d7d9c9f5de23f7f4
Artyom Kazak <yom@artyom.me>
2018-10-20 20:17:56 +0200

    Done

--- servant-mock/src/Mock.hs
+++ servant-mock/src/Mock.hs
@@ -2,25 +2,35 @@
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE InstanceSigs #-}
+{-# LANGUAGE TypeApplications #-}
 
 module Mock
     ( ClientMock
     ) where
 
 import Data.Text (Text)
+import Data.Text.Encoding (decodeUtf8)
+import Data.ByteString (ByteString)
 import Control.Monad.Reader
 import Control.Concurrent.MVar
 import Control.Exception
+import Control.DeepSeq (rnf)
+import GHC.Exts (fromList)
 
-import qualified Data.ByteString.Builder as Builder
 import qualified Network.Wai as Wai
 import qualified Network.Wai.Internal as Wai
 import qualified Network.HTTP.Types as HTTP
+import qualified Network.HTTP.Client as HTTP
+
+import qualified Data.ByteString.Char8 as BC
+import qualified Data.ByteString.Lazy as BSL
+import qualified Data.ByteString.Builder as Builder
 
 import Servant
 import Servant.API
 import Servant.Client
 import Servant.Client.Core
+import Servant.Client.Internal.HttpClient (requestToClientRequest)
 
 -- We'd like to be able to perform requests without doing actual networking.
 -- If we have a WAI 'Application', we can do just that, so the following
@@ -47,16 +57,14 @@ instance RunClient ClientMock where
         -- However, since we "execute" this response by converting it to a
         -- servant-client 'Response', we don't care if the response becomes
         -- invalid.
-        responseMVar <- newEmptyMVar
+        responseMVar <- newEmptyMVar @Response
         let storeResponse :: Wai.Response -> IO Wai.ResponseReceived
             storeResponse waiResponse = do
-                let response = fromWaiResponse
-                                   (requestHttpVersion request)
-                                   waiResponse
+                let response = fromWaiResponse waiResponse
                 evaluate response
                 putMVar responseMVar response
                 pure Wai.ResponseReceived
-        Wai.ResponseReceived <- app request storeResponse
+        Wai.ResponseReceived <- app (toWaiRequest request) storeResponse
         takeMVar responseMVar
 
     streamingRequest :: Request -> ClientMock StreamingResponse
@@ -73,35 +81,63 @@ instance RunClient ClientMock where
 -- 'toWaiRequest' makes a Servant request consumable by a WAI 'Application'.
 toWaiRequest :: Request -> Wai.Request
 toWaiRequest request =
-    let clientRequest = requestToClientRequest request in
     Wai.defaultRequest
-        { Wai.requestMethod  = method clientRequest
-        , Wai.rawPathInfo    = path clientRequest
-        , Wai.pathInfo       = path clientRequest
-        , Wai.rawQueryString = queryString clientRequest
-        , Wai.queryString    = queryString clientRequest
-        , Wai.requestHeaders = requestHeaders clientRequest
-        , Wai.requestBody    = requestBody clientRequest
-        , Wai.isSecure       = secure clientRequest
+        { Wai.requestMethod  = HTTP.method clientRequest
+        , Wai.rawPathInfo    = HTTP.path clientRequest
+        , Wai.pathInfo       = map decodeUtf8 . BC.split '/' . BC.drop 1 $
+                                 HTTP.path clientRequest
+        , Wai.rawQueryString = HTTP.queryString clientRequest
+        , Wai.queryString    = HTTP.parseQuery $
+                                 HTTP.queryString clientRequest
+        , Wai.requestHeaders = HTTP.requestHeaders clientRequest
+        , Wai.requestBody    = toWaiRequestBody $
+                                 HTTP.requestBody clientRequest
+        , Wai.isSecure       = HTTP.secure clientRequest
         -- NB: the 'remoteHost' is doing to be localhost in our case, since
-        -- we're doing all requests locally.
+        -- we're doing all requests locally. It's already a default in
+        -- 'defaultRequest', therefore we don't include 'HTTP.host' as a
+        -- field here.
         }
+  where
+    localhost = BaseUrl Https "0.0.0.0" 80 ""
+    clientRequest = requestToClientRequest localhost request
+
+-- 'toWaiRequestBody' converts a http-client representation of a request
+-- body to a simpler one employed by WAI.
+--
+-- The proof-of-concept only handles the most common response type.
+toWaiRequestBody :: HTTP.RequestBody -> IO ByteString
+toWaiRequestBody body = case body of
+    HTTP.RequestBodyLBS body' ->
+        pure (BSL.toStrict body')
+    HTTP.RequestBodyBS body' ->
+        pure body'
+    HTTP.RequestBodyBuilder _ body' ->
+        pure (BSL.toStrict (Builder.toLazyByteString body'))
+    HTTP.RequestBodyIO body' ->
+        toWaiRequestBody =<< body'
+    HTTP.RequestBodyStream{} ->
+        error "toWaiRequestBody: RequestBodyStream yet unsupported"
+    HTTP.RequestBodyStreamChunked{} ->
+        error "toWaiRequestBody: RequestBodyStreamChunked yet unsupported"
 
 -- 'fromWaiResponse' executes a response and ensures that once the Servant
 -- response has been evaluated, the original WAI response can be safely
 -- discarded.
+--
+-- The proof-of-concept only handles the most common response type.
 fromWaiResponse :: Wai.Response -> Response
 fromWaiResponse (Wai.ResponseBuilder status headers builder) =
     let body = Builder.toLazyByteString builder in
     rnf body `seq` Response
         { responseStatusCode = status
-        , responseHeaders = headers
+        , responseHeaders = fromList headers
         , responseHttpVersion = HTTP.http11  -- always HTTP/1.1 for servant-client
         , responseBody = body
         }
 fromWaiResponse Wai.ResponseFile{} =
-    error "fromWaiResponse: implement for 'ResponseFile' is needed"
+    error "fromWaiResponse: ResponseFile yet unsupported"
 fromWaiResponse Wai.ResponseStream{} =
-    error "fromWaiResponse: implement for 'ResponseStream' is needed"
+    error "fromWaiResponse: ResponseStream yet unsupported"
 fromWaiResponse Wai.ResponseRaw{} =
-    error "fromWaiResponse: implement for 'ResponseRaw' is needed"
+    error "fromWaiResponse: ResponseRaw yet unsupported"
